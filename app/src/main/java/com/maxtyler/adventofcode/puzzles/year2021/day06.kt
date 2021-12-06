b6a01241632c023f8ca9a2345d597a3f30bf080b
Max Tyler <maxastyler@gmail.com>
2021-12-06 11:58:21 +0000

    Update day 6

--- app/src/main/java/com/maxtyler/adventofcode/puzzles/year2021/day06.kt
+++ app/src/main/java/com/maxtyler/adventofcode/puzzles/year2021/day06.kt
@@ -2,18 +2,19 @@ package com.maxtyler.adventofcode.puzzles.year2021
 
 import com.maxtyler.adventofcode.puzzles.Puzzle
 
-fun step(fish: List<Long>): List<Long> =
-    (fish.drop(1) + (fish[0] + fish[7])).mapIndexed { index, i -> if (index == 6) i + fish[0] else i }
+fun step(fish: List<Long>): List<Long> = fish.mapIndexed { i, l ->
+    when {
+        i == 6 -> fish[0] + fish[7]
+        i == 15 -> fish[0] + fish[7]
+        else -> fish[i + 1]
+    }
+}
 
 object day06 : Puzzle<List<Long>> {
 
     override fun parseInput(input: String) =
         Regex("(\\d+)").findAll(input).map { it.value.toInt() }.groupingBy { it }.eachCount()
-            .let { counts ->
-                val countArray = MutableList(7 + 9) { 0L }
-                counts.forEach { i, v -> countArray[i] = v.toLong() }
-                countArray
-            }
+            .let { counts -> MutableList(7 + 9) { counts[it]?.toLong() ?: 0L } }
 
     override fun part1(input: List<Long>): String {
         return generateSequence(input, ::step).drop(80).first().sum().toString()
