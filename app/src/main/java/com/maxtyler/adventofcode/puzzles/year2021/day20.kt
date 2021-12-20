ac2852fd91096cff6956faf41797f37902b00221
Max Tyler <maxastyler@gmail.com>
2021-12-20 14:42:20 +0000

    Clean up day 20

--- app/src/main/java/com/maxtyler/adventofcode/puzzles/year2021/day20.kt
+++ app/src/main/java/com/maxtyler/adventofcode/puzzles/year2021/day20.kt
@@ -17,30 +17,17 @@ object day20 : Puzzle<Pair<Set<Int>, Map<Pair<Int, Int>, Int>>> {
         lut: Set<Int>,
         image: Map<Pair<Int, Int>, Int>,
         outsideState: Int
-    ): Map<Pair<Int, Int>, Int> {
-        val minI = image.keys.minOf { it.first }
-        val maxI = image.keys.maxOf { it.first }
-        val minJ = image.keys.minOf { it.second }
-        val maxJ = image.keys.maxOf { it.second }
-        return (minI - 1..maxI + 1).flatMap { i ->
-            (minJ - 1..maxJ + 1).map { j ->
-                (i to j) to (1 downTo -1).flatMap { di ->
-                    (1 downTo -1).map { dj ->
-                        val a = i + di
-                        val b = j + dj
-                        image.getOrDefault(a to b, outsideState)
-                    }
-                }.foldIndexed(0) { i, a, v -> (v shl i) or a }
-            }
-        }.associate { it.first to if (it.second in lut) 1 else 0 }
-    }
-
-    fun displayMap(map: Map<Pair<Int, Int>, Int>) =
-        (map.keys.minOf { it.first }..map.keys.maxOf { it.first }).map { i ->
-            (map.keys.minOf { it.second }..map.keys.maxOf { it.second }).map { j ->
-                if (map[i to j] == 1) '#' else ' '
-            }.joinToString("")
-        }.joinToString("\n")
+    ) = (image.keys.minOf { it.first } - 1..image.keys.maxOf { it.first } + 1).flatMap { i ->
+        (image.keys.minOf { it.second } - 1..image.keys.maxOf { it.second } + 1).map { j ->
+            (i to j) to (1 downTo -1).flatMap { di ->
+                (1 downTo -1).map { dj ->
+                    val a = i + di
+                    val b = j + dj
+                    image.getOrDefault(a to b, outsideState)
+                }
+            }.foldIndexed(0) { i, a, v -> (v shl i) or a }
+        }
+    }.associate { it.first to if (it.second in lut) 1 else 0 }
 
     fun mapSequence(initialMap: Map<Pair<Int, Int>, Int>, lut: Set<Int>) =
         generateSequence(initialMap to 0) { (m, i) ->
--- app/src/test/java/com/maxtyler/adventofcode/puzzles/year2021/day17Test.kt
+++ app/src/test/java/com/maxtyler/adventofcode/puzzles/year2021/day17Test.kt
@@ -1,10 +1,11 @@
 package com.maxtyler.adventofcode.puzzles.year2021
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class day17Test {
     val input = "target area: x=20..30, y=-10..-5"
+
     @Test
     fun part1Test() {
         assertEquals("45", day17.part1(day17.parseInput(input)))
